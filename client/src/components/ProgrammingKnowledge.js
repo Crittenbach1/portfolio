22d69fb6f50c915a4e18a1c7a7c3351858bdd903
crittenbach1 <crittenbach@gmail.com>
2019-02-03 05:55:43 -0500

    contains all home page main components

---
+++ client/src/components/ProgrammingKnowledge.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProgrammingKnowledge extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Programming Knowledge</h1>
+      </div>
+    );
+  }
+}
+
+export default ProgrammingKnowledge;
